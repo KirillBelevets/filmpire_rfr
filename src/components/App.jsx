5a0a9eabefc6ef1105f479dbc79f6bd8154df202
Kirill Belevets <kirill.belevets@gmail.com>
2025-01-28 11:35:47 +0200

    add file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Switch, Route } from "react-router-dom";
+
+import useStyles from "./styles";
+
+import { Actors, Movies, MovieInformation, NavBar, Profile } from "./";
 
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route path="/movies/:id">
+            <MovieInformation />
+          </Route>
+          <Route path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route path="/">
+            <Movies />
+          </Route>
+          <Route path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 };
 
 export default App;
